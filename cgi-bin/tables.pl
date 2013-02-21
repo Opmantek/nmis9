10f04648830f0a24a1d187f065ab892b68b6aff7
Mark Dueck <markd@opmantek.com>
2013-02-21 16:04:04 +1000

    get the last of the changes for status renamed to serviceStatus
    Conflicts:
    	cgi-bin/menu.pl
    	install/Status.nmis

--- cgi-bin/tables.pl
+++ cgi-bin/tables.pl
@@ -174,7 +174,7 @@ sub loadCfgTable {
 			{ group => { header => 'Group',display => 'header,popup',value => [ @groups] }},
 			{ location => { header => 'Location',display => 'header,popup',value => [ @locations] }},
 			{ businessService => { header => 'Business Service',display => 'header,pop',value => [ @businessServices ] }},
-			{ status => { header => 'Status',display => 'header,popup',value => [ @serviceStatus ] }},
+			{ serviceStatus => { header => 'Service Status',display => 'header,popup',value => [ @serviceStatus ] }},
 			{ model => { header => 'Model',display => 'header,popup',value => [@models] }},
 			{ active => { header => 'Active',display => 'header,popup',value => ["true", "false"] }},
 			{ ping => { header => 'Ping', display => 'header,popup',value => ["true", "false"] }},
