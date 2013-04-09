b64166036475e480ffe0cf470ff980b59daae3e9
Keith Sinclair <keiths@opmantek.com>
2013-04-09 13:54:01 +1000

    Updated manual interface discovery

--- admin/alcatel_interface_discovery.pl
+++ admin/alcatel_interface_discovery.pl
@@ -77,22 +77,32 @@ sub processNode {
 		# Initiase the system object and load a node.
 		my $S = Sys::->new; # get system object
 		$S->init(name=>$node,snmp=>'false'); # load node info and Model if name exists
+		$S->readNodeView; # from doUpdate  
 		my $NI = $S->ndinfo;
 		my $NCT = loadNodeConfTable();
 		my $NC = $S->ndcfg;
+		my $V = $S->view;
+
 
 		############################################################################################################
 		#
 		## THe following code contains psuedo-code inside it.  Just the first and second if statements
 		#
 		############################################################################################################
-		if ( $NI->{system}{sysDescr} =~ /ARAM|ISAM/ and $NI->{system}{nodeVendor} eq "ALCATEL" ) {
+		if ( $NI->{system}{sysDescr} =~ /ASAM|ARAM|ISAM/ and $NI->{system}{nodeVendor} eq "Alcatel Data Network" ) {
 			#asamActiveSoftware1	standby
 			#asamActiveSoftware2	active
 			#asamSoftwareVersion1	/OSWP/OSWPAA37.432
 			#asamSoftwareVersion2	OSWP/66.98.63.71/OSWPAA41.353/OSWPAA41.353
 			my $asamVersion41 = "OSWPAA37.432";
-			my $asamVersion42 = "OSWPAA41.353";
+			my $asamVersion41 = "OSWPAA41.353";
+			my $asamVersion42 = "OSWPAA41.363";
+			
+			my $rack_count = 1;
+			my $shelf_count = 1;
+
+			$rack_count = $LNT->{$node}{rack_count} if $LNT->{$node}{rack_count} ne "";
+			$shelf_count = $LNT->{$node}{shelf_count} if $LNT->{$node}{shelf_count} ne "";
 			
 			my $asamSoftwareVersion = $S->{info}{system}{asamSoftwareVersion1};
 			if ( $S->{info}{system}{asamActiveSoftware2} eq "active" ) {
@@ -107,11 +117,11 @@ sub processNode {
 				# How to identify it is an ARAM-D?
 				#"For ARAM-D with extensions "
 				if( 0 ) {
-					my $indexes = build_41_interface_indexes( shelf => 2 );
+					my $indexes = build_41_interface_indexes( shelf_count => $shelf_count, rack_count => $rack_count );
 					@ifIndexNum = @{$indexes};
 				}
 				else {
-					my $indexes = build_41_interface_indexes( shelf => 1 );
+					my $indexes = build_41_interface_indexes( shelf_count => $shelf_count, rack_count => $rack_count );
 					@ifIndexNum = @{$indexes};
 				}
 				
@@ -119,8 +129,11 @@ sub processNode {
 			#" release 4.2  ( ISAM FD y  ISAM-V) "
 			elsif( $asamSoftwareVersion eq $asamVersion42 )
 			{
-				my $indexes = build_42_interface_indexes();
+				my $indexes = build_42_interface_indexes( shelf_count => $shelf_count, rack_count => $rack_count );
 				@ifIndexNum = @{$indexes};
+			}
+			else {
+				print STDERR "WHAT!  asamSoftwareVersion=$asamSoftwareVersion\n";
 			}
 
 		
@@ -129,26 +142,26 @@ sub processNode {
 
 			foreach my $index (@ifIndexNum) {
 				$intfTotal++;				
-				my $ifDescr = "Port $index Gigabit Ethernet";
+				my $ifDescr = "ATM $index";
 				$S->{info}{interface}{$index} = {
 		      'Description' => '',
-		      'ifAdminStatus' => 'up',
+		      'ifAdminStatus' => 'unknown',
 		      'ifDescr' => $ifDescr,
 		      'ifIndex' => $index,
 		      'ifLastChange' => '0:00:00',
 		      'ifLastChangeSec' => 0,
-		      'ifOperStatus' => 'up',
+		      'ifOperStatus' => 'unknown',
 		      'ifSpeed' => 1000000000,
-		      'ifType' => 'ethernetCsmacd',
-		      'interface' => "port-$index-gigabit-ethernet",
+		      'ifType' => 'atm',
+		      'interface' => "atm-$index",
 		      'real' => 'true',
 		      'threshold' => 'true'
 				
 				};
 				
 				# preset collect,event to required setting, Node Configuration Will override.
-				$S->{info}{interface}{$index}{collect} = "true";
-				$S->{info}{interface}{$index}{event} = "true";
+				$S->{info}{interface}{$index}{collect} = "false";
+				$S->{info}{interface}{$index}{event} = "false";
 									
 				# ifDescr must always be filled
 				if ($S->{info}{interface}{$index}{ifDescr} eq "") { $S->{info}{interface}{$index}{ifDescr} = $index; }
@@ -156,7 +169,7 @@ sub processNode {
 				foreach my $i (keys %{$S->{info}{interface}}) {
 					if ($index ne $i and $S->{info}{interface}{$index}{ifDescr} eq $S->{info}{interface}{$i}{ifDescr}) {
 						$S->{info}{interface}{$index}{ifDescr} = "$S->{info}{interface}{$index}{ifDescr}-${index}"; # add index to string
-						$S->{view}{interface}{"${index}_ifDescr_value"} = $S->{info}{interface}{$index}{ifDescr}; # update
+						$V->{interface}{"${index}_ifDescr_value"} = $S->{info}{interface}{$index}{ifDescr}; # update
 						dbg("Interface Description changed to $S->{info}{interface}{$index}{ifDescr}");
 					}
 				}
@@ -164,14 +177,14 @@ sub processNode {
 				### warning - will overwrite what we got from the device - be warned !!!
 				if ($NCT->{$S->{node}}{$ifDescr}{Description} ne '') {
 					$S->{info}{interface}{$index}{nc_Description} = $S->{info}{interface}{$index}{Description}; # save
-					$S->{info}{interface}{$index}{Description} = $S->{view}{interface}{"${index}_Description_value"} = $NCT->{$S->{node}}{$ifDescr}{Description};
+					$S->{info}{interface}{$index}{Description} = $V->{interface}{"${index}_Description_value"} = $NCT->{$S->{node}}{$ifDescr}{Description};
 					dbg("Manual update of Description by nodeConf");
 				}
 				if ($NCT->{$S->{node}}{$ifDescr}{ifSpeed} ne '') {
 					$S->{info}{interface}{$index}{nc_ifSpeed} = $S->{info}{interface}{$index}{ifSpeed}; # save
-					$S->{info}{interface}{$index}{ifSpeed} = $S->{view}{interface}{"${index}_ifSpeed_value"} = $NCT->{$S->{node}}{$ifDescr}{ifSpeed};
+					$S->{info}{interface}{$index}{ifSpeed} = $V->{interface}{"${index}_ifSpeed_value"} = $NCT->{$S->{node}}{$ifDescr}{ifSpeed};
 					### 2012-10-09 keiths, fixing ifSpeed to be shortened when using nodeConf
-					$S->{view}{interface}{"${index}_ifSpeed_value"} = convertIfSpeed($S->{info}{interface}{$index}{ifSpeed});
+					$V->{interface}{"${index}_ifSpeed_value"} = convertIfSpeed($S->{info}{interface}{$index}{ifSpeed});
 					dbg("Manual update of ifSpeed by nodeConf");
 				}
 				
@@ -224,37 +237,49 @@ sub processNode {
 				$NI->{system}{intfCollect} = $intfCollect;
 		
 				# prepare values for web page
-				$S->{view}{interface}{"${index}_ifDescr_value"} = $S->{info}{interface}{$index}{ifDescr};
+				$V->{interface}{"${index}_ifDescr_value"} = $S->{info}{interface}{$index}{ifDescr};
 
-				$S->{view}{interface}{"${index}_event_value"} = $S->{info}{interface}{$index}{event};
-				$S->{view}{interface}{"${index}_event_title"} = 'Event on';
+				$V->{interface}{"${index}_event_value"} = $S->{info}{interface}{$index}{event};
+				$V->{interface}{"${index}_event_title"} = 'Event on';
 		
-				$S->{view}{interface}{"${index}_threshold_value"} = $NC->{node}{threshold} ne 'true' ? 'false': $S->{info}{interface}{$index}{threshold};
-				$S->{view}{interface}{"${index}_threshold_title"} = 'Threshold on';
+				$V->{interface}{"${index}_threshold_value"} = $NC->{node}{threshold} ne 'true' ? 'false': $S->{info}{interface}{$index}{threshold};
+				$V->{interface}{"${index}_threshold_title"} = 'Threshold on';
 		
-				$S->{view}{interface}{"${index}_collect_value"} = $S->{info}{interface}{$index}{collect};
-				$S->{view}{interface}{"${index}_collect_title"} = 'Collect on';
+				$V->{interface}{"${index}_collect_value"} = $S->{info}{interface}{$index}{collect};
+				$V->{interface}{"${index}_collect_title"} = 'Collect on';
 		
 				# collect status
-				delete $S->{view}{interface}{"${index}_nocollect_title"};
+				delete $V->{interface}{"${index}_nocollect_title"};
 				if ($S->{info}{interface}{$index}{collect} eq "true") {
 					dbg("ifIndex $index, collect=true");
 				} else {
-					$S->{view}{interface}{"${index}_nocollect_value"} = $S->{info}{interface}{$index}{nocollect};
-					$S->{view}{interface}{"${index}_nocollect_title"} = 'Reason';
+					$V->{interface}{"${index}_nocollect_value"} = $S->{info}{interface}{$index}{nocollect};
+					$V->{interface}{"${index}_nocollect_title"} = 'Reason';
 					dbg("ifIndex $index, collect=false, $S->{info}{interface}{$index}{nocollect}");
 					# no collect => no event, no threshold
-					$S->{info}{interface}{$index}{threshold} = $S->{view}{interface}{"${index}_threshold_value"} = 'false';
-					$S->{info}{interface}{$index}{event} = $S->{view}{interface}{"${index}_event_value"} = 'false';
+					$S->{info}{interface}{$index}{threshold} = $V->{interface}{"${index}_threshold_value"} = 'false';
+					$S->{info}{interface}{$index}{event} = $V->{interface}{"${index}_event_value"} = 'false';
 				}
 		
 				# get color depending of state
-				$S->{view}{interface}{"${index}_ifAdminStatus_color"} = getAdminColor(sys=>$S,index=>$index);
-				$S->{view}{interface}{"${index}_ifOperStatus_color"} = getOperColor(sys=>$S,index=>$index);
+				$V->{interface}{"${index}_ifAdminStatus_color"} = getAdminColor(sys=>$S,index=>$index);
+				$V->{interface}{"${index}_ifOperStatus_color"} = getOperColor(sys=>$S,index=>$index);
+
+				$V->{interface}{"${index}_ifAdminStatus_value"} = $S->{info}{interface}{$index}{ifAdminStatus};
+				$V->{interface}{"${index}_ifOperStatus_value"} = $S->{info}{interface}{$index}{ifOperStatus};
 		
+				# Add the titles as they are missing from the model.
+				$V->{interface}{"${index}_ifOperStatus_title"} = 'Oper Status';
+				$V->{interface}{"${index}_ifDescr_title"} = 'Name';
+				$V->{interface}{"${index}_ifSpeed_title"} = 'Bandwidth';
+				$V->{interface}{"${index}_ifType_title"} = 'Type';
+				$V->{interface}{"${index}_ifAdminStatus_title"} = 'Admin Status';
+				$V->{interface}{"${index}_ifLastChange_title"} = 'Last Change';
+				$V->{interface}{"${index}_Description_title"} = 'Description';
+
 				# index number of interface
-				$S->{view}{interface}{"${index}_ifIndex_value"} = $index;
-				$S->{view}{interface}{"${index}_ifIndex_title"} = 'ifIndex';
+				$V->{interface}{"${index}_ifIndex_value"} = $index;
+				$V->{interface}{"${index}_ifIndex_title"} = 'ifIndex';
 			}
 			
 			#print Dumper $S;
@@ -267,26 +292,37 @@ sub processNode {
 
 sub build_41_interface_indexes {
 	my %args = @_;
-	my $rack = 1;
-	my $shelf = $arg{shelf};
+	my $rack_count = 1;
+	my $shelf_count = 1;
 
 	# For ARAM-D with extensions the shelf value changes to 2 for the first extension (shelf = 010) , 3 for the second (shelf = 11) … and so on, 
 	# such that the first port of the first card of the first extension would be:
-	if( defined( $args{shelf} ) ) {
-		$shelf = $args{shelf};
+	if( defined( $args{rack_count} ) ) {
+		$rack_count = $args{rack_count};
 	}
-	my $level = 0;
 
+	if( defined( $args{shelf_count} ) ) {
+		$shelf_count = $args{shelf_count};
+	}
+
+	my $level = 3;
+
+	my @racks = (1..$rack_count);
+	my @shelves = (1..$shelf_count);
 	my @slots = (3..19);
 	my @circuits = (0..47);
 	
 	my @interfaces = ();
 
-	foreach my $slot (@slots) {
-		foreach my $circuit (@circuits) {
-			my $index = generate_interface_index_41 ( rack => $rack, shelf => $shelf, slot => $slot, level => $level, circuit => $circuit);
-			push( @interfaces, $index );
-		}		
+	foreach my $rack (@racks) {
+		foreach my $shelf (@shelves) {
+			foreach my $slot (@slots) {
+				foreach my $circuit (@circuits) {
+					my $index = generate_interface_index_41 ( rack => $rack, shelf => $shelf, slot => $slot, level => $level, circuit => $circuit);
+					push( @interfaces, $index );
+				}		
+			}
+		}
 	}
 	return \@interfaces;
 }
