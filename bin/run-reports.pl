3598cf692b93d67199a18833f3c0099b40ba4323
Keith Sinclair <keiths@opmantek.com>
2014-01-16 17:22:38 +1000

    Adding a script to fix copyright dates

--- bin/run-reports.pl
+++ bin/run-reports.pl
@@ -2,21 +2,31 @@
 #
 ## $Id: run-reports.pl,v 8.1 2013/01/03 07:42:50 keiths Exp $
 #
-#	Copyright 2008 (C) Network Management Information Systems Ltd
-#	<www.nmis.co.nz/contact>, hereafter referred to as 'NMIS'.
-#	PO Box 42149, Christchurch 8042, New Zealand.
-#	Contributed by Eric Greenwood <eric_at_nmis.co.nz> and Jan van Keulen
-#	<jan_at_nmis.co.nz>
+#  Copyright Opmantek Limited (www.opmantek.com)
 #
-#	This file, run-reports.pl, is part of NMIS. NMIS is free software: you can
-#	redistribute it and/or modify it under the terms of the GNU General
-#	Public License as published by the Free Software Foundation, either
-#	version 3 of the License, or (at your option) any later version. NMIS is
-#	distributed in the hope that it will be useful, but WITHOUT ANY
-#	WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-#	more details. You should have received a copy of the GNU General Public
-#	License along with NMIS. If not, see <http://www.gnu.org/licenses/>.
+#  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
+#
+#  This file is part of Network Management Information System ("NMIS").
+#
+#  NMIS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  NMIS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with NMIS (most likely in a file named LICENSE).
+#  If not, see <http://www.gnu.org/licenses/>
+#
+#  For further information on NMIS or for a license other than GPL please see
+#  www.opmantek.com or email contact@opmantek.com
+#
+#  User group details:
+#  http://support.opmantek.com/users/
 #
 # *****************************************************************************
 
