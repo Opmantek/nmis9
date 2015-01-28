94b48926c9c78b8cd1f818af1c192afaf34e3f69
Alexander Zangerl <alexz@opmantek.com>
2015-01-28 17:36:14 +1000

    removed long-ago commented 'use image::resize' which the installer misunderstands as 'we must have image::resize', which is a mostly useless and very marginal module.

--- lib/opmantek_rrdfunc.pm
+++ lib/opmantek_rrdfunc.pm
@@ -269,15 +269,6 @@ sub rrdFetchGraphPData {
 		# ($begin,$step,$names,$data) = RRDs::graphfetch($tmpimg, @options);
 		if ( -f $tmpimg ) {
 	
-			### 2012-01-30 keiths, deprecating the need for Win32 support and Image::Resize.
-			#use Image::Resize;
-			#my $image = Image::Resize->new("$tmpimg");
-			#my $gd = $image->resize(120,120);
-	
-			#open(FH, ">$tmpimg");
-			#print FH $gd->png();
-			#close(FH);
-	
 			open(IMG,"$tmpimg") or logMsg("$NI->{system}{name}, ERROR: problem with $tmpimg; $!");
 			binmode(IMG);
 			binmode(STDOUT);
