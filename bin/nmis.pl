2484e94f99f510b96d4a062c56c9060aad0942a3
Alexander Zangerl <alexz@opmantek.com>
2016-08-22 11:08:09 +1000

    fixed the breakage to nmis.pl that commit 6b9bf3 somehow introduced
    unknown why the merge went so wrong for keith, but somehow or other it
    reverted most of the type=purge code and broke the polltime/updatetime startup
    parroting code.

--- bin/nmis.pl
+++ bin/nmis.pl
@@ -41,10 +41,12 @@ use Net::SNMP qw(oid_lex_sort);
 use Proc::ProcessTable;
 use Proc::Queue ':all';
 use Data::Dumper;
+use File::Find;
+use File::Spec;
 use Statistics::Lite qw(mean);
 use POSIX qw(:sys_wait_h);
-# this imports the LOCK_ *constants (eg. LOCK_UN, LOCK_EX)
-use Fcntl qw(:DEFAULT :flock);
+# this imports the LOCK_ *constants (eg. LOCK_UN, LOCK_EX), also the stat modes
+use Fcntl qw(:DEFAULT :flock :mode);
 use Errno qw(EAGAIN ESRCH EPERM);
 
 use NMIS;
@@ -170,6 +172,7 @@ elsif ( $type eq "rme" ) { loadRMENodes($rmefile); }
 elsif ( $type eq "threshold" ) { runThreshold($node); printRunTime(); } # included in type=collect
 elsif ( $type eq "master" ) { nmisMaster(); printRunTime(); } # included in type=collect
 elsif ( $type eq "groupsync" ) { sync_groups(); }
+elsif ( $type eq "purge" ) { my $error = purge_files(); die "$error\n" if $error; }
 else { checkArgs(); }
 
 exit;
@@ -806,6 +809,15 @@ sub doUpdate
 	my $updatetime = $updatetimer->elapTime();
 	info("updatetime for $name was $updatetime");
 	$reachdata->{updatetime} = { value => $updatetime, option => "gauge,0:U,".(86400*3) };
+	# parrot the previous reading's poll time
+	my $prevval = "U";
+	if (my $rrdfilename = $S->getDBName(type => "health"))
+	{
+		my $infohash =RRDs::info($rrdfilename);
+		$prevval = $infohash->{'ds[polltime].last_ds'} if (defined $infohash->{'ds[polltime].last_ds'});
+	}
+	$reachdata->{polltime} = { value => $prevval, option => "gauge,0:U," };
+
 	updateRRD(sys=>$S, data=> $reachdata, type=>"health");
 	$S->close;
 
@@ -1064,6 +1076,15 @@ sub doCollect {
 	my $polltime = $pollTimer->elapTime();
 	info("polltime for $name was $polltime");
 	$reachdata->{polltime} = { value =>  $polltime, option => "gauge,0:U" };
+	# parrot the previous reading's update time
+	my $prevval = "U";
+	if (my $rrdfilename = $S->getDBName(type => "health"))
+	{
+		my $infohash =RRDs::info($rrdfilename);
+		$prevval = $infohash->{'ds[updatetime].last_ds'} if (defined $infohash->{'ds[updatetime].last_ds'});
+	}
+	$reachdata->{updatetime} = { value => $prevval, option => "gauge,0:U,".(86400*3) };
+
 	updateRRD(sys=>$S, data=> $reachdata, type=>"health");
 	$S->close;
 
@@ -5292,6 +5313,8 @@ sub runReach
 	my $swapMax = 0;
 	my $diskMax = 0;
 
+	my %reach;
+
 	info("Starting node $NI->{system}{name}, type=$NI->{system}{nodeType}");
 
 	# Math hackery to convert Foundry CPU memory usage into appropriate values
@@ -5328,11 +5351,13 @@ sub runReach
 		}
 	}
 
-	# copy results from object
+	# copy stashed results (produced by runPing and getnodeinfo)
 	my $pingresult = $RI->{pingresult};
+	$reach{responsetime} = $RI->{pingavg};
+	$reach{loss} = $RI->{pingloss};
+
 	my $snmpresult = $RI->{snmpresult};
 
-	my %reach;		# copy in local table
 	$reach{cpu} = $RI->{cpu};
 	$reach{mem} = $RI->{mem};
 	if ( $RI->{swap} ) {
@@ -5342,8 +5367,6 @@ sub runReach
 	if ( defined $RI->{disk} and $RI->{disk} > 0 ) {
 		$reach{disk} = $RI->{disk};
 	}
-	$reach{responsetime} = $RI->{pingavg};
-	$reach{loss} = $RI->{pingloss};
 	$reach{operStatus} = $RI->{operStatus};
 	$reach{operCount} = $RI->{operCount};
 
@@ -7473,7 +7496,7 @@ sub printCrontab
 
 	my $usercol = getbool($nvp{system})? "\troot\t" : '';
 
-	print <<EO_TEXT;
+	print qq|
 # if you DON'T want any NMIS cron mails to go to root,
 # uncomment and adjust the next line
 # MAILTO=WhoeverYouAre\@yourdomain.tld
@@ -7491,7 +7514,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # */3 * * * * $usercol $C->{'<nmis_base>'}/bin/nmis.pl type=services mthread=true maxthreads=10
 ######################################################
 # Run Summary Update every 2 minutes
-*/2 * * * * $usercol /usr/local/nmis8/bin/nmis.pl type=summary
+*/2 * * * * $usercol $C->{'<nmis_base>'}/bin/nmis.pl type=summary
 #####################################################
 # Run the interfaces 4 times an hour with Thresholding on!!!
 # if threshold_poll_cycle is set to false, then enable cron based thresholding
@@ -7504,10 +7527,10 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # the installer offers to setup using install/logrotate*.conf
 #
 # backup configuration, models and crontabs once a day, and keep 30 backups
-22 8 * * * $usercol $C->{'<nmis_base>'}/admin/config_backup.pl $C->{'<nmis_data>'}/backups 30
+22 8 * * * $usercol $C->{'<nmis_base>'}/admin/config_backup.pl $C->{'<nmis_backups>'} 30
 ##################################################
-# purge old files every week
-0 2 * * 0 $usercol $C->{'<nmis_base>'}/admin/nmis_file_cleanup.sh $C->{'<nmis_base>'} 30
+# purge old files every few days
+2 2 */3 * * $usercol $C->{'<nmis_base>'}/bin/nmis.pl type=purge
 ########################################
 # Run the Reports Weekly Monthly Daily
 # daily
@@ -7530,7 +7553,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 40 2 1 * * $usercol $C->{'<nmis_base>'}/bin/run-reports.pl month response
 50 2 1 * * $usercol $C->{'<nmis_base>'}/bin/run-reports.pl month avail
 ###########################################
-EO_TEXT
+|;
 }
 
 #=========================================================================================
@@ -7736,6 +7759,7 @@ command line options are:
       links     Generate the links.csv file.
       rme       Read and generate a node.csv file from a Ciscoworks RME file
       groupsync Check all nodes and add any missing groups to the configuration
+      purge     Remove old files, or print them if simulate=true
   [conf=<file name>]     Optional alternate configuation file in conf directory
   [node=<node name>]     Run operations on a single node;
   [group=<group name>]   Run operations on all nodes in the named group;
