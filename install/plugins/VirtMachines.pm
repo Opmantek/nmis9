0b75abc0a6058cc14350fa12784abc214772982e
Alexander Zangerl <alexz@opmantek.com>
2017-08-09 15:40:59 +1000

    nmis9: updated virtmachines plugin to work with nmis9

--- install/plugins/VirtMachines.pm
+++ install/plugins/VirtMachines.pm
@@ -27,40 +27,56 @@
 #  
 # *****************************************************************************
 #
-# a small update plugin for adding links to the vmware guests if they're managed by nmis
+# a small update plugin for adding links to the vmware guests 
+# if they're managed by nmis
 
 package VirtMachines;
-our $VERSION = "1.0.1";
+our $VERSION = "2.0.0";
 
 use strict;
-use NMISNG::Util;												# for the conf table extras
-use Compat::NMIS;
 
 sub update_plugin
 {
 	my (%args) = @_;
 	my ($node,$S,$C) = @args{qw(node sys config)};
 
-	my $NI = $S->ndinfo;
-	# anything to do?
-	return (0,undef) if (ref($NI->{VirtMachines}) ne "HASH");
+	# does this node host virtual machines?
+	my $vmitems = $S->nmisng_node->get_inventory_ids(
+		concept => "VirtMachines",
+		filter => { historic => 0 });
+	
+	return (0,undef) if (!@$vmitems);
 	my $changesweremade = 0;
 
-	info("Working on $node VirtMachines");
-	
-	my $LNT = Compat::NMIS::loadLocalNodeTable();
+	$S->nmisng->log->info("Working on $node VirtMachines");
 
-	for my $vm (keys %{$NI->{VirtMachines}})
+	for my $vmid (@$vmitems)
 	{
-		my $entry = $NI->{VirtMachines}{$vm};
-		my $vmName = 	$entry->{vmwVmDisplayName};
-				
-		if ( defined $LNT->{$vmName} and defined $LNT->{$vmName}{name} and $LNT->{$vmName}{name} eq $vmName ) {
-			$changesweremade = 1;
-			$entry->{vmwVmDisplayName_url} = "/cgi-nmis8/network.pl?conf=$C->{conf}&act=network_node_view&node=$vmName";
-			$entry->{vmwVmDisplayName_id} = "node_view_$vmName";
+		my ($vminventory,$error) = $S->nmisng_node->inventory(_id => $vmid);
+		if ($error)
+		{
+			$S->nmisng->log->error("Failed to get inventory $vmid: $error");
+			next;
 		}
+		
+		my $vmdata = $vminventory->data;	# r/o copy, must be saved back if changed
+		my $vmName = 	$vmdata->{vmwVmDisplayName};
 
+		# is there a managed node with the given vmwVmDisplayName?
+		if ( defined $S->nmisng->node(name => $vmName) )
+		{
+			$changesweremade = 1;
+
+			$S->nmisng->log->debug("Updating VM linkage for $vmName");
+			$vmdata->{vmwVmDisplayName_url} 
+			= "/cgi-nmis8/network.pl?conf=$C->{conf}&act=network_node_view&node=$vmName";
+			$vmdata->{vmwVmDisplayName_id} = "node_view_$vmName";
+
+			$vminventory->data($vmdata); # set changed info
+			(undef,$error) = $vminventory->save;
+			$S->nmisng->log->error("failed to save inventory for $vmid: $error")
+					if ($error);
+		}
 	}
 	return ($changesweremade,undef); # report if we changed anything
 }
