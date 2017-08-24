e873e11a44ca0fb0f1e22c8e57192bab4fc5ee1c
Alexander Zangerl <alexz@opmantek.com>
2017-08-24 15:37:53 +1000

    nmis9: fix nmisng::sys::prep_extras_with_catchalls to use convertIfName on ifdescr from inventory

--- lib/NMISNG/Sys.pm
+++ lib/NMISNG/Sys.pm
@@ -1841,14 +1841,15 @@ sub prep_extras_with_catchalls
 				# no fallback to info section as interface update is running
 				# $data = $self->{info}{interface}{$indx} if(defined($self->{info}{interface}) && defined($self->{info}{interface}{$indx}));
 				$data = $interface_inventory->data();
-				foreach my $key (qw(ifAlias Description ifDescr ifType))
+				foreach my $key (qw(ifAlias Description ifType))
 				{
 					$extras->{$key} ||= $interface_inventory->$key();
+					
 				}
-				$extras->{ifDescr} ||= NMISNG::Util::convertIfName( $extras->{ifDescr} );
+				$extras->{ifDescr} ||=  NMISNG::Util::convertIfName($interface_inventory->ifDescr());
 				$extras->{ifMaxOctets} ||= $interface_inventory->max_octets();
 				$extras->{maxBytes}    ||= $interface_inventory->max_bytes();
-				$extras->{maxPackets}  ||= $interface_inventory->max_packets();
+				$extras->{maxPackets}  ||= $interface_inventorny->max_packets();
 				$extras->{ifSpeedIn}   ||= $interface_inventory->ifSpeedIn();
 				$extras->{ifSpeedOut}  ||= $interface_inventory->ifSpeedOut();
 				$extras->{ifSpeed} ||= $interface_inventory->ifSpeed();
