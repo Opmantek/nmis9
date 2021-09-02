11369212abf9b4cd20311b5af23cfad579942b91
Robin Twiggs <robint@opmantek.com>
2021-09-02 18:58:01 +1000

    Updates models-install/Common-Teldat-cbqos.nmis Adds models-install/Model-Teldati60-uses-common.nmis Updates conf-default/plugins/TeldatQoSStattable.pm Updates conf-default/plugins/TeldatQoSStattable.pm Updates conf-default/plugins/QualityOfServiceStattable.pm Updates models-install/Model.nmis Updates models-default/Common-database.nmis

--- conf-default/plugins/QualityOfServiceStattable.pm
+++ conf-default/plugins/QualityOfServiceStattable.pm
@@ -1,36 +1,36 @@
 #
 #  Copyright Opmantek Limited (www.opmantek.com)
-#  
+#
 #  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
-#  
+#
 #  This file is part of Network Management Information System ("NMIS").
-#  
+#
 #  NMIS is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  NMIS is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
-#  along with NMIS (most likely in a file named LICENSE).  
+#  along with NMIS (most likely in a file named LICENSE).
 #  If not, see <http://www.gnu.org/licenses/>
-#  
+#
 #  For further information on NMIS or for a license other than GPL please see
-#  www.opmantek.com or email contact@opmantek.com 
-#  
+#  www.opmantek.com or email contact@opmantek.com
+#
 #  User group details:
 #  http://support.opmantek.com/users/
-#  
+#
 # *****************************************************************************
 #
-# a small update plugin for getting the QualityOfServiceStat index and direction
+# a small update plugin for manipulating QualityOfServiceStat
 
 package QualityOfServiceStattable;
-our $VERSION = "1.0.4";
+our $VERSION = "2.0.0";
 
 use strict;
 use warnings;
@@ -41,13 +41,22 @@ use Data::Dumper;
 sub update_plugin
 {
 	my (%args) = @_;
-	my ($node,$S,$C,$NG) = @args{qw(node sys config nmisng)};
+	my ($node, $S, $C, $NG) = @args{qw(node sys config nmisng)};
 
-	$NG->log->info("QualityOfServiceStattable.pm:update: Running for node $node");
+	my $sub = 'update';
+	my $plugin = 'QualityOfServiceStattable.pm';
+	my $concept = 'QualityOfServiceStat';
+	my $inventory_data_key = 'index';
+
+	$NG->log->info("$plugin:$sub: Running for node $node");
 
 	# anything to do?
-	# this plugin deals only with QualityOfServiceStat
-	return (0,undef) if (ref($S->{mdl}{systemHealth}{sys}{QualityOfServiceStat}) ne "HASH");
+	# this plugin deals only with $concept
+	# let's not auto-vivify anything...
+	return (0,undef) if (ref($S->{mdl}) ne "HASH"
+				or ref($S->{mdl}->{systemHealth}) ne "HASH"
+				or ref($S->{mdl}->{systemHealth}->{sys}) ne "HASH"
+				or ref($S->{mdl}{systemHealth}{sys}{$concept}) ne "HASH");
 
 	$NG->log->debug9("\$node: ".Dumper \$node);
 	$NG->log->debug9("\$S: ".Dumper \$S);
@@ -56,28 +65,27 @@ sub update_plugin
 
 	my $changesweremade = 0;
 
-	my $concept = 'QualityOfServiceStat';
 	my $ids = $S->nmisng_node->get_inventory_ids(
 		concept => $concept,
 		filter => { historic => 0 });
 
 	if (@$ids)
 	{
-		$NG->log->debug9("QualityOfServiceStattable.pm:update: \$ids: ".Dumper $ids);
-		$NG->log->debug9("QualityOfServiceStattable.pm:update: \$S->{mdl}{systemHealth}{sys}{QualityOfServiceStat}: ".Dumper \%{$S->{mdl}{systemHealth}{sys}{QualityOfServiceStat}});
+		$NG->log->debug9("$plugin:$sub: \$ids: ".Dumper $ids);
+		$NG->log->debug9("$plugin:$sub: \$S->{mdl}{systemHealth}{sys}{$concept}: ".Dumper \%{$S->{mdl}{systemHealth}{sys}{$concept}});
 
-        # for linkage lookup this needs the interfaces inventory as well, but
-        # a non-object r/o copy of just the data (no meta) is enough
-        # we don't want to re-query multiple times for the same interface...
-        my $result = $S->nmisng_node->get_inventory_model(concept => "interface",
-                                                          filter => { historic => 0 });
-        if (my $error = $result->error)
-        {
-                $NG->log->error("QualityOfServiceStattable.pm:update: Failed to get inventory: $error");
-                return(0,undef);
-        }
-        my %ifdata =  map { ($_->{data}->{index} => $_->{data}) } (@{$result->data});
-		$NG->log->debug9("QualityOfServiceStattable.pm:update: \%ifdata: ".Dumper \%ifdata);
+		# for linkage lookup this needs the interfaces inventory as well, but
+		# a non-object r/o copy of just the data (no meta) is enough
+		# we don't want to re-query multiple times for the same interface...
+		my $result = $S->nmisng_node->get_inventory_model(concept => "interface",
+		                                                  filter => { historic => 0 });
+		if (my $error = $result->error)
+		{
+		        $NG->log->error("$plugin:$sub: Failed to get inventory: $error");
+		        return(0,undef);
+		}
+		my %ifdata =  map { ($_->{data}->{index} => $_->{data}) } (@{$result->data});
+		$NG->log->debug9("$plugin:$sub: \%ifdata: ".Dumper \%ifdata);
 
 		for my $id (@$ids)
 		{
@@ -94,16 +102,15 @@ sub update_plugin
 
 			$NG->log->debug("id '$id' \$inventory_data:\n".Dumper($inventory_data));
 
-			# |  +--hwCBQoSPolicyStatisticsClassifierEntry(1)
-			# |     |	hwCBQoSIfApplyPolicyIfIndex ($ifindex) ,
-			#		 	hwCBQoSIfVlanApplyPolicyVlanid1 (undef),
-			#			hwCBQoSIfApplyPolicyDirection ($third),
-			#			hwCBQoSPolicyStatClassifierName ($k is the index that derives from this property)
-			# $k is a QualityOfServiceStat index provided by hwCBQoSPolicyStatClassifierName
-			#	and is a dot delimited string of integers (OID):
-			my ($ifindex,undef,$third) = split(/\./,$inventory_data->{index});
-			if ( defined($ifindex) or defined ($third) )
+			if ( defined($inventory_data->{$inventory_data_key}) )
 			{
+				my ($ifindex,undef,$third) = split(/\./,$inventory_data->{$inventory_data_key});
+				if (! defined($ifindex))
+				{
+					$NG->log->info("$plugin:$sub: \$ifindex not defined.");
+					return(0,undef);
+				}
+
 				$changed = 1;
 
 				$NG->log->debug("defined \$ifindex = '$ifindex'.");
@@ -117,24 +124,28 @@ sub update_plugin
 				$inventory_data->{ifIndex} = $ifindex;
 				$inventory_data->{Direction} = $direction;
 
-				$NG->log->info("Found QoS Entry with interface $inventory_data->{ifIndex}. 'ifIndex' = '$inventory_data->{ifIndex}'.");
-				$NG->log->info("Found QoS Entry with interface $inventory_data->{Direction}. 'ifIndex' = '$inventory_data->{Direction}'.");
+				$NG->log->info("$plugin:$sub: Found $concept entry with $inventory_data_key '$inventory_data->{$inventory_data_key}': 'ifIndex' = '$inventory_data->{ifIndex}'.");
+				$NG->log->info("$plugin:$sub: Found $concept entry with $inventory_data_key '$inventory_data->{$inventory_data_key}': 'Direction' = '$inventory_data->{Direction}'.");
 
-				$NG->log->debug("QualityOfServiceStattable.pm:update: Node $node updating node info QualityOfServiceStat $inventory_data->{index} ifIndex: new '$inventory_data->{ifIndex}'");
-				$NG->log->debug("QualityOfServiceStattable.pm:update: Node $node updating node info QualityOfServiceStat $inventory_data->{index} Direction: new '$inventory_data->{Direction}'");
+				$NG->log->debug("$plugin:$sub: Node $node updating node info $concept $inventory_data_key '$inventory_data->{$inventory_data_key}': ifIndex: new '$inventory_data->{ifIndex}'");
+				$NG->log->debug("$plugin:$sub: Node $node updating node info $concept $inventory_data_key '$inventory_data->{$inventory_data_key}': Direction: new '$inventory_data->{Direction}'");
 
 				# Get the devices ifDescr.
 				if ( defined $ifdata{$ifindex}{ifDescr} )
 				{
 					$inventory_data->{ifDescr} = $ifdata{$ifindex}{ifDescr};
 
-					$NG->log->info("Found QoS Entry with interface $inventory_data->{ifIndex}. 'ifDescr' = '$inventory_data->{ifDescr}'.");
-					$NG->log->debug("QualityOfServiceStattable.pm:update: Node $node updating node info QualityOfServiceStat $inventory_data->{index}: new '$inventory_data->{ifDescr}'");
+					$NG->log->info("$plugin:$sub: Found $concept entry with $inventory_data_key '$inventory_data->{$inventory_data_key}': 'ifDescr' = '$inventory_data->{ifDescr}'.");
+					$NG->log->debug("$plugin:$sub: Node $node updating node info $concept $inventory_data_key '$inventory_data->{$inventory_data_key}': new '$inventory_data->{ifDescr}'");
 				}
 				else
 				{
-					$NG->log->info("\$ifdata{$ifindex}{ifDescr} not defined. 'ifDescr' could not be determined for ifIndex '$ifindex'.");
+					$NG->log->info("$plugin:$sub: \$ifdata{$ifindex}{ifDescr} not defined. 'ifDescr' could not be determined for $concept entry with $inventory_data_key '$inventory_data->{$inventory_data_key}'.");
 				}
+			}
+			else
+			{
+				$NG->log->info("$plugin:$sub: \$inventory_data->{$inventory_data_key} not defined. 'ifDescr' could not be determined for '$inventory_data->{$inventory_data_key}'");
 			}
 
 			if ($changed)
@@ -143,18 +154,19 @@ sub update_plugin
 				(undef,$error) = $inventory->save; # and save to the db
 				if ($error)
 				{
-					$NG->log->error("Failed to save inventory for $concept index ".$inventory_data->{index}. " : $error");
+					$NG->log->error("$plugin:$sub: Failed to save inventory for $concept $inventory_data_key ".$inventory_data->{$inventory_data_key}. " : $error");
 				}
 				else
 				{
 					$changesweremade = 1;
+					$NG->log->debug("id '$id' updated \$inventory_data:\n".Dumper($inventory_data));
 				}
 			}
 		}
 	}
 	else
 	{
-		$NG->log->debug("QualityOfServiceStattable.pm:update: 'if(\@\$ids)' returned false");
+		$NG->log->debug("$plugin:$sub: 'if(\@\$ids)' returned false");
 	}
 
 	return ($changesweremade,undef); # report if we changed anything
--- conf-default/plugins/TeldatBRSStattable.pm
+++ conf-default/plugins/TeldatBRSStattable.pm
@@ -1,36 +1,36 @@
 #
 #  Copyright Opmantek Limited (www.opmantek.com)
-#  
+#
 #  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
-#  
+#
 #  This file is part of Network Management Information System ("NMIS").
-#  
+#
 #  NMIS is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  NMIS is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
-#  along with NMIS (most likely in a file named LICENSE).  
+#  along with NMIS (most likely in a file named LICENSE).
 #  If not, see <http://www.gnu.org/licenses/>
-#  
+#
 #  For further information on NMIS or for a license other than GPL please see
-#  www.opmantek.com or email contact@opmantek.com 
-#  
+#  www.opmantek.com or email contact@opmantek.com
+#
 #  User group details:
 #  http://support.opmantek.com/users/
-#  
+#
 # *****************************************************************************
 #
-# a small update plugin for getting the TeldatBRSStat ifDescr
+# a small update plugin for manipulating TeldatBRSStat
 
 package TeldatBRSStattable;
-our $VERSION = "1.0.1";
+our $VERSION = "2.0.0";
 
 use strict;
 use warnings;
@@ -40,14 +40,23 @@ use Data::Dumper;
 
 sub update_plugin
 {
-        my (%args) = @_;
-        my ($node,$S,$C,$NG) = @args{qw(node sys config nmisng)};
+	my (%args) = @_;
+	my ($node, $S, $C, $NG) = @args{qw(node sys config nmisng)};
 
-	$NG->log->info("TeldatBRSStattable.pm:update: Running for node $node");
+	my $sub = 'update';
+	my $plugin = 'TeldatBRSStattable.pm';
+	my $concept = 'TeldatBRSStat';
+	my $inventory_data_key = 'ifIndex';
+
+	$NG->log->info("$plugin:$sub: Running for node $node");
 
 	# anything to do?
-	# this plugin deals only with TeldatBRSStat
-	return (0,undef) if (ref($S->{mdl}{systemHealth}{sys}{TeldatBRSStat}) ne "HASH");
+	# this plugin deals only with $concept
+	# let's not auto-vivify anything...
+	return (0,undef) if (ref($S->{mdl}) ne "HASH"
+				or ref($S->{mdl}->{systemHealth}) ne "HASH"
+				or ref($S->{mdl}->{systemHealth}->{sys}) ne "HASH"
+				or ref($S->{mdl}{systemHealth}{sys}{$concept}) ne "HASH");
 
 	$NG->log->debug9("\$node: ".Dumper \$node);
 	$NG->log->debug9("\$S: ".Dumper \$S);
@@ -56,28 +65,27 @@ sub update_plugin
 
 	my $changesweremade = 0;
 
-	my $concept = 'TeldatBRSStat';
 	my $ids = $S->nmisng_node->get_inventory_ids(
 		concept => $concept,
 		filter => { historic => 0 });
 
 	if (@$ids)
 	{
-		$NG->log->debug9("TeldatBRSStattable.pm:update: \$ids: ".Dumper $ids);
-		$NG->log->debug9("TeldatBRSStattable.pm:update: \$S->{mdl}{systemHealth}{sys}{TeldatBRSStat}: ".Dumper \%{$S->{mdl}{systemHealth}{sys}{TeldatBRSStat}});
+		$NG->log->debug9("$plugin:$sub: \$ids: ".Dumper $ids);
+		$NG->log->debug9("$plugin:$sub: \$S->{mdl}{systemHealth}{sys}{$concept}: ".Dumper \%{$S->{mdl}{systemHealth}{sys}{$concept}});
 
-	        # for linkage lookup this needs the interfaces inventory as well, but
-	        # a non-object r/o copy of just the data (no meta) is enough
-	        # we don't want to re-query multiple times for the same interface...
-	        my $result = $S->nmisng_node->get_inventory_model(concept => "interface",
-	                                                          filter => { historic => 0 });
-	        if (my $error = $result->error)
-	        {
-	                $NG->log->error("TeldatBRSStattable.pm:update: Failed to get inventory: $error");
-	                return(0,undef);
-	        }
-	        my %ifdata =  map { ($_->{data}->{index} => $_->{data}) } (@{$result->data});
-		$NG->log->debug9("TeldatBRSStattable.pm:update: \%ifdata: ".Dumper \%ifdata);
+		# for linkage lookup this needs the interfaces inventory as well, but
+		# a non-object r/o copy of just the data (no meta) is enough
+		# we don't want to re-query multiple times for the same interface...
+		my $result = $S->nmisng_node->get_inventory_model(concept => "interface",
+		                                                  filter => { historic => 0 });
+		if (my $error = $result->error)
+		{
+		        $NG->log->error("$plugin:$sub: Failed to get inventory: $error");
+		        return(0,undef);
+		}
+		my %ifdata =  map { ($_->{data}->{index} => $_->{data}) } (@{$result->data});
+		$NG->log->debug9("$plugin:$sub: \%ifdata: ".Dumper \%ifdata);
 
 		for my $id (@$ids)
 		{
@@ -94,28 +102,30 @@ sub update_plugin
 
 			$NG->log->debug("id '$id' \$inventory_data:\n".Dumper($inventory_data));
 
-			if ( defined($inventory_data->{ifIndex}) )
+			if ( defined($inventory_data->{$inventory_data_key}) )
 			{
-				my $ifindex = $inventory_data->{ifIndex};
-			
-	                        # Get the devices ifDescr.
-	                        if ( defined $ifdata{$ifindex}{ifDescr} )
+				my $ifindex = $inventory_data->{$inventory_data_key};
+
+				$NG->log->debug("defined \$ifindex = '$ifindex'.");
+
+				# Get the devices ifDescr.
+				if ( defined $ifdata{$ifindex}{ifDescr} )
 				{
 					$changed = 1;
 
-	                                $inventory_data->{ifDescr} = $ifdata{$ifindex}{ifDescr};
+					$inventory_data->{ifDescr} = $ifdata{$ifindex}{ifDescr};
 
-					$NG->log->info("Found QoS Entry with interface $inventory_data->{ifIndex}. 'ifDescr' = '$inventory_data->{ifDescr}'.");
-					$NG->log->debug("TeldatBRSStattable.pm:update: Node $node updating node info TeldatBRSStat $inventory_data->{index}: new '$inventory_data->{ifDescr}'");
+					$NG->log->info("$plugin:$sub: Found $concept entry with $inventory_data_key '$inventory_data->{$inventory_data_key}': 'ifDescr' = '$inventory_data->{ifDescr}'.");
+					$NG->log->debug("$plugin:$sub: Node $node updating node info $concept $inventory_data_key '$inventory_data->{$inventory_data_key}': new '$inventory_data->{ifDescr}'");
 				}
 				else
 				{
-					$NG->log->info("\$ifdata{$ifindex}{ifDescr} not defined. 'ifDescr' could not be determined for ifIndex '$ifindex'.");
+					$NG->log->info("$plugin:$sub: \$ifdata{$ifindex}{ifDescr} not defined. 'ifDescr' could not be determined for $concept entry with $inventory_data_key '$inventory_data->{$inventory_data_key}'.");
 				}
 			}
 			else
 			{
-				$NG->log->info("\$inventory_data->{ifIndex} not defined. 'ifDescr' could not be determined for '$inventory_data->{index}'");
+				$NG->log->info("$plugin:$sub: \$inventory_data->{$inventory_data_key} not defined. 'ifDescr' could not be determined for '$inventory_data->{$inventory_data_key}'");
 			}
 
 			if ($changed)
@@ -124,18 +134,19 @@ sub update_plugin
 				(undef,$error) = $inventory->save; # and save to the db
 				if ($error)
 				{
-					$NG->log->error("Failed to save inventory for $concept index ".$inventory_data->{index}. " : $error");
+					$NG->log->error("$plugin:$sub: Failed to save inventory for $concept $inventory_data_key ".$inventory_data->{$inventory_data_key}. " : $error");
 				}
 				else
 				{
 					$changesweremade = 1;
+					$NG->log->debug("id '$id' updated \$inventory_data:\n".Dumper($inventory_data));
 				}
 			}
 		}
 	}
 	else
 	{
-		$NG->log->debug("TeldatBRSStattable.pm:update: 'if(\@\$ids)' returned false");
+		$NG->log->debug("$plugin:$sub: 'if(\@\$ids)' returned false");
 	}
 
 	return ($changesweremade,undef); # report if we changed anything
