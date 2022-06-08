1a7936d69b800821aee43e355615f4ae8f39dc56
Keith Sinclair <keiths@opmantek.com>
2022-06-08 13:23:43 +1000

    Adding Cisco IronPort ESA models from Mitchell Saunders @ FirstWave

---
+++ conf-default/plugins/InterfaceTable.pm
@@ -0,0 +1,188 @@
+#
+#  Copyright Opmantek Limited (www.opmantek.com)
+#
+#  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
+#
+#  This file is part of Network Management Information System ("NMIS").
+#
+#  NMIS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  NMIS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with NMIS (most likely in a file named LICENSE).
+#  If not, see <http://www.gnu.org/licenses/>
+#
+#  For further information on NMIS or for a license other than GPL please see
+#  www.opmantek.com or email contact@opmantek.com
+#
+#  User group details:
+#  http://support.opmantek.com/users/
+#
+# *****************************************************************************
+#
+# a small update plugin for manipulating QualityOfServiceStat
+
+package InterfaceTable;
+our $VERSION = "1.0.0";
+
+use strict;
+use warnings;
+###use diagnostics;
+use lib "$FindBin::Bin/../../lib";
+use NMISNG::rrdfunc;
+
+use Data::Dumper;
+
+sub update_plugin
+{
+	my (%args) = @_;
+	my ($node, $S, $C, $NG) = @args{qw(node sys config nmisng)};
+
+	my $sub = 'update';
+	my $plugin = 'InterfaceTable.pm';
+	my $concept = 'InterfaceTable';
+	my $inventory_data_key = 'index';
+
+	$NG->log->info("$plugin:$sub: Running for node $node");
+
+    my $nodeobj = $NG->node(name => $node);
+    my $inv = $S->inventory( concept => 'catchall' );
+	my $catchall_data = $inv->data;
+
+	my $IF = $nodeobj->ifinfo;	
+	my $MDL = $S->mdl;
+            
+	my $NC = $nodeobj->configuration;
+
+	$NG->log->debug9("\$node: ".Dumper \$nodeobj);
+	$NG->log->debug9("\$S: ".Dumper \$S);
+	$NG->log->debug9("\$C: ".Dumper \$C);
+	$NG->log->debug9("\$NG: ".Dumper \$NG);
+
+	my %nodeconfig = %{$NC};
+
+	return (1,undef) if ( $catchall_data->{nodeModel} ne "CiscoESA" or !NMISNG::Util::getbool($catchall_data->{collect}));
+
+	my $changesweremade = 0;
+
+	my $ids = $S->nmisng_node->get_inventory_ids(
+		concept => $concept,
+		filter => { historic => 0 });
+
+	if (@$ids)
+	{
+		$NG->log->debug9("$plugin:$sub: \$ids: ".Dumper $ids);
+		$NG->log->debug9("$plugin:$sub: \$S->{mdl}{systemHealth}{sys}{$concept}: ".Dumper \%{$S->{mdl}{systemHealth}{sys}{$concept}});
+
+        my $active = 0;
+        
+        for my $ifTableId (@$ids)
+        {
+                my ($ifTable, $error) = $S->nmisng_node->inventory(_id => $ifTableId);
+                if ($error)
+                {
+                    $NG->log->error("Failed to get inventory $ifTableId: $error");
+                    next;
+                }
+                my $data = $ifTable->data();
+          
+                my $interface_data;
+                foreach my $w (qw(index Description ifAdminStatus ifDescr ifHighSpeed ifLastChange ifOperStatus ifSpeed ifType)) {
+                    $interface_data->{$w} = $data->{$w};
+                }
+                $interface_data->{ifIndex} = $data->{index};
+                $interface_data->{collect} = "true";
+                $interface_data->{interface} = $data->{ifDescr};
+                $interface_data->{Description} = $data->{ifDescr};
+								
+                if ($data->{ifAdminStatus} eq "up") {
+                   $active++;
+                }
+                
+                my $path = $nodeobj->inventory_path( concept => 'interface', path_keys => [], data => $interface_data );
+
+                my ($inventory,$error_message) = $nodeobj->inventory(
+                    concept => 'interface',
+                    path => $path,
+                    path_keys => [],
+                    create => 1
+                );
+                $NG->log->error("Failed to get inventory for device_global, error_message:$error_message")
+                        if(!$inventory);
+                # create is set so we should have an inventory here
+                if($inventory)
+                {
+                    # not sure why supplying the data above does not work, needs a test!
+                    $inventory->data( $interface_data );
+                    $inventory->historic(0);
+                    $inventory->enabled(1);
+                    # disable for now
+                    $inventory->data_info( subconcept => 'interface', enabled => 0 );
+                    my ($op,$error) = $inventory->save();
+                    $NG->log->debug2( "saved ".join(',', @$path)." op: $op");
+                    $NG->log->info( "saved ".join(',', @$path)." op: $op");
+                } else {
+                    $NG->log->error("No inventory");
+                }
+                $NG->nmisng->log->error("Failed to save inventory, error_message:$error") if($error);
+        }
+        $changesweremade = 1;
+        $catchall_data->{ifNumber} = $ids;
+        $catchall_data->{active} = $active;
+        $inv->data($catchall_data);
+        my ($op,$error) = $inv->save();
+        $NG->log->info( "saved catchall op: $op");
+        
+	}
+	else
+	{
+		$NG->log->debug("$plugin:$sub: 'if(\@\$ids)' returned false");
+	}
+
+	return ($changesweremade,undef); # report if we changed anything
+}
+
+sub collect_plugin
+{
+	my (%args) = @_;
+	my ($node, $S, $C, $NG) = @args{qw(node sys config nmisng)};
+	
+	my @knownindices;
+	my $changesweremade = 0;
+	return (0,undef) if ($S->{mdl}->{system}->{nodeModel} ne "NL-Aviat");
+
+	my $IFitems = $S->nmisng_node->get_inventory_ids(
+		concept => "interface");
+	
+				
+	if (@$IFitems)
+	{
+		for my $ifTableId (@$IFitems)
+    {
+       my ($ifTable, $error) = $S->nmisng_node->inventory(_id => $ifTableId);
+       if ($error)
+       {
+           $NG->log->error("Failed to get inventory $ifTableId: $error");
+           next;
+       }
+       my $data = $ifTable->data();
+			$ifTable->historic(0);
+			$ifTable->data($data);
+			my ($op, $error) = $ifTable->save();
+			#$NG->log->debug2("Inventory update: $op ");
+			
+		}
+	}
+	$changesweremade = 1;
+	return ($changesweremade,undef);
+}
+
+
+1;
