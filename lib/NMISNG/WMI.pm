3f3a4544bcf0e6b01022305a8b6e45664b67a5e8
Mark Dueck <mark.dueck@firstwave.com>
2023-11-10 07:25:51 +1000

    Initial pass adding wmic_server support.
    Not tested

--- lib/NMISNG/WMI.pm
+++ lib/NMISNG/WMI.pm
@@ -35,7 +35,6 @@ our $tmp     = "$ENV{NMISTMPDIR}" || "/tmp";
 use strict;
 use File::Temp;
 use Encode 2.23;								# core module, version is what came with 5.10.0 which we can make do with
-
 # the constructor is not doing much at this time, merely checks that the arguments are sufficient
 #
 # args: host, username, (required), password, timeout, program (optional),
@@ -181,163 +180,204 @@ sub _run_query
 	my (@rawdata, $exitcode, %result, $version, $cmdLine);
 	my $v2option = "";
 
-	# fork and pipe
-	my $pid = open(WMIC, "-|");
-	if (!defined $pid)
+	if (!$self->{version})
 	{
-		unlink $cmdfn or warn "Could not remove file '$cmdfn' Error: $!";
-		unlink $tfn or warn "Could not remove file '$tfn' Error: $!";
-		unlink $authfn or warn "Could not remove file '$authfn' Error: $!";
-		return (error => "cannot fork to run wmic: $!");
+		$version = "Version 1";
 	}
-	elsif ($pid)
-	{
-		# parent: save and restore any previously running alarm,
-		# but don't bother subtracting time spent here
-		my $remaining = alarm(0);
-		eval
-		{
-			local $SIG{ALRM} = sub { die "alarm\n"; };
-			alarm($timeout) if ($timeout); # setup execution timeout
 
-			close $tfh;									# not ours to use
-			close $authfh;
-			@rawdata = <WMIC>;					# read the goodies from the child
-			close(WMIC);
-			$exitcode = $?;
-			alarm(0);
-		};
-		alarm($remaining) if ($remaining);
-		if ($@ and $@ eq "alarm\n")
+	if( $self->{version} eq 'wmic_server' ) 
+	{
+		require Mojo::UserAgent;
+		my $headers = {"Content-type" => 'application/json', Accept => 'application/json'};
+		my $client  = Mojo::UserAgent->new();
+		my $url     = Mojo::URL->new('http://192.168.88.150:2313/wmic');
+		my $id = $self->{username};
+		my $token = 'MYSECRETUSERACCESSTOKEN1'; # // $self->{password};
+		my $host = $self->{host};
+		my $namespace = "root/cimv2";
+		my $post_json = { id => $id, token => $token, host => $host, query => $query, namespace => $namespace };
+
+		my $res = $client->post($url => $headers => json => $post_json )->result;
+		if ($res->is_success) 
 		{
-			# don't want the wmic process to hang around, we stopped consuming its output
-			# and it can't do anything useful anymore
-			kill("KILL",$pid);
+			my $json = $res->json;
+			if( $json ) 
+			{
+				my ($classname, @fieldnames, %nicedata);
+				# we need to get the classname, wmic returns it, wmic_server does not so use the last word/token in the query 
+				# assuming it will be the 'table' which seems to be the classname				
+				my @words = split(' ', $query);
+				$classname = $words[-1];
+				
+				foreach my $entry (@$json) 
+				{
+					$nicedata{$classname} ||= [];
+					push @{$nicedata{$classname}}, $entry;
+				}
+				$result{ok} = 1;
+				$result{data} = \%nicedata;
+			} else { $result{error} = "Error no json found, found body ".$res->body."  getting wmic_server url $url"; }
+		}
+		elsif ($res->is_error) { $result{error} = "Error message ".$res->message." occurred getting wmic_server url $url"; }
+		elsif ($res->code != 200) { $result{error} = "Error code ".$res->code." occurred getting wmic_server url $url"; }
+		else { $result{error} = "Unknown error occurred getting wmic_server url $url"; }		
+	}
+	else {
+		# Handle Version 1 and Version 2 which are wmic executable
+		# fork and pipe
+		my $pid = open(WMIC, "-|");
+		if (!defined $pid)
+		{
 			unlink $cmdfn or warn "Could not remove file '$cmdfn' Error: $!";
 			unlink $tfn or warn "Could not remove file '$tfn' Error: $!";
 			unlink $authfn or warn "Could not remove file '$authfn' Error: $!";
-			return (error => "timeout after $timeout seconds");
+			return (error => "cannot fork to run wmic: $!");
 		}
-	}
-	else
-	{
-		# child
-		open(STDIN, "</dev/null");
-		open(STDERR, '>&'.$tfh);		# stderr to go there, please
-
-		# -A format is badly documented. smbclient manpage has a little bit of info
-		# however, unclear if that password can be quoted or contain spaces or the like...
-
-		# let's accept usernames with domains, as user@domain or domain/user
-		my $foundDomain = 0;
-		if (!$self->{version})
+		elsif ($pid)
 		{
-			$version = "Version 1";
-		}
-		if ($self->{username})
-		{
-		    if ($self->{username} =~ m!^([^/@]+)([/@])(.+)$!)
-		    {
-			    my ($user,$delim,$domain) = ($1,$2,$3);
-			    ($user,$domain) = ($domain,$user) if ($delim eq "/");
-			    print $authfh "username=$user\ndomain=$domain\n";
-			    $foundDomain = 1;
-		    }
-		    else
-		    {
-		        print $authfh "username=$self->{username}\n";
-		    }
-		}
-		if ($self->{domain} && !$foundDomain)
-		{
-			print $authfh "domain=$self->{domain}\n";
-		}
-		print $authfh "password=$self->{password}\n" if ($self->{password});
-		close $authfh;
+			# parent: save and restore any previously running alarm,
+			# but don't bother subtracting time spent here
+			my $remaining = alarm(0);
+			eval
+			{
+				local $SIG{ALRM} = sub { die "alarm\n"; };
+				alarm($timeout) if ($timeout); # setup execution timeout
 
-		$cmdLine  = "$self->{program} --delimiter=$delim -A $authfn";
-		$cmdLine .= " --option='client ntlmv2 auth'=Yes" if ($self->{version} eq 'Version 2');
-		$cmdLine .= " --no-pass" if (!$self->{password});
-		$cmdLine .= " //".$self->{host};
-		$cmdLine .= " '$query' | grep -v dcerpc_pipe_connect";
-
-		print $cmdfh "$cmdLine";
-		close($cmdfh);
-		exec($cmdLine);
-		die "exec failed: Command: '$cmdLine' Code $!\n";
-	}
-
-	# failed? read tempfile for error msgs
-	if ($exitcode)
-	{
-		$result{error} = "wmic failed: ";
-		if (-s $cmdfn && open(F, $cmdfn)) # has data
-		{
-			$result{error} .= "Command: '" . <F> . "' ";
-			close(F);
-		}
-		if (-s $tfn && open(F, $tfn)) # has data
-		{
-			$result{error} .= join(" ", <F>);
-			close(F);
+				close $tfh;									# not ours to use
+				close $authfh;
+				@rawdata = <WMIC>;					# read the goodies from the child
+				close(WMIC);
+				$exitcode = $?;
+				alarm(0);
+			};
+			alarm($remaining) if ($remaining);
+			if ($@ and $@ eq "alarm\n")
+			{
+				# don't want the wmic process to hang around, we stopped consuming its output
+				# and it can't do anything useful anymore
+				kill("KILL",$pid);
+				unlink $cmdfn or warn "Could not remove file '$cmdfn' Error: $!";
+				unlink $tfn or warn "Could not remove file '$tfn' Error: $!";
+				unlink $authfn or warn "Could not remove file '$authfn' Error: $!";
+				return (error => "timeout after $timeout seconds");
+			}
 		}
 		else
 		{
-			$result{error} .= " exit code ".($exitcode>>8);
-		}
-		# remove new lines in the error message
-		$result{error} =~ s/\n/\\n/;
-		unlink $cmdfn or warn "Could not remove file '$cmdfn' Error: $!";
-		unlink $tfn or warn "Could not remove file '$tfn' Error: $!";
-		unlink $authfn or warn "Could not remove file '$authfn' Error: $!";
-	}
-	else
-	{
-		unlink $cmdfn or warn "Could not remove file '$cmdfn' Error: $!";
-		unlink $tfn or warn "Could not remove file '$tfn' Error: $!";
-		unlink $authfn or warn "Could not remove file '$authfn' Error: $!";
-		# worked? extract class, fieldnames
-		# produce hash for each class, array of subhashes for the rows
-		my ($classname, @fieldnames, %nicedata);
-		for my $line (@rawdata)
-		{
-			chomp $line;
-			# wmic may very well return utf-8 encoded data, e.g. for Caption in win32_operatingsystem on 6.0.6001
-			# so let's try to be nice and decode it into native unicode
-			my $validunicode = eval { decode('utf-8', $line, Encode::FB_CROAK); };
-			$line = $validunicode if (!$@);
+			# child
+			open(STDIN, "</dev/null");
+			open(STDERR, '>&'.$tfh);		# stderr to go there, please
 
-			# CLASS: Win32_PerfRawData_PerfOS_PagingFile
-			if ($line =~ /^class:\s+(\S+)\s*$/i)
+			# -A format is badly documented. smbclient manpage has a little bit of info
+			# however, unclear if that password can be quoted or contain spaces or the like...
+
+			# let's accept usernames with domains, as user@domain or domain/user
+			my $foundDomain = 0;
+			if ($self->{username})
 			{
-				$classname = $1;
-				undef @fieldnames;	# next line must be field names
+					if ($self->{username} =~ m!^([^/@]+)([/@])(.+)$!)
+					{
+						my ($user,$delim,$domain) = ($1,$2,$3);
+						($user,$domain) = ($domain,$user) if ($delim eq "/");
+						print $authfh "username=$user\ndomain=$domain\n";
+						$foundDomain = 1;
+					}
+					else
+					{
+							print $authfh "username=$self->{username}\n";
+					}
+			}
+			if ($self->{domain} && !$foundDomain)
+			{
+				print $authfh "domain=$self->{domain}\n";
+			}
+			print $authfh "password=$self->{password}\n" if ($self->{password});
+			close $authfh;
+
+			$cmdLine  = "$self->{program} --delimiter=$delim -A $authfn";
+			$cmdLine .= " --option='client ntlmv2 auth'=Yes" if ($self->{version} eq 'Version 2');
+			$cmdLine .= " --no-pass" if (!$self->{password});
+			$cmdLine .= " //".$self->{host};
+			$cmdLine .= " '$query' | grep -v dcerpc_pipe_connect";
+
+			print $cmdfh "$cmdLine";
+			close($cmdfh);
+			exec($cmdLine);
+			die "exec failed: Command: '$cmdLine' Code $!\n";
+		}
+
+		# failed? read tempfile for error msgs
+		if ($exitcode)
+		{
+			$result{error} = "wmic failed: ";
+			if (-s $cmdfn && open(F, $cmdfn)) # has data
+			{
+				$result{error} .= "Command: '" . <F> . "' ";
+				close(F);
+			}
+			if (-s $tfn && open(F, $tfn)) # has data
+			{
+				$result{error} .= join(" ", <F>);
+				close(F);
 			}
 			else
 			{
-				# should be either list of names or list of values, with delim
-				my @columns = split(qr/$delim/, $line);
-				if (!@fieldnames)
+				$result{error} .= " exit code ".($exitcode>>8);
+			}
+			# remove new lines in the error message
+			$result{error} =~ s/\n/\\n/;
+			unlink $cmdfn or warn "Could not remove file '$cmdfn' Error: $!";
+			unlink $tfn or warn "Could not remove file '$tfn' Error: $!";
+			unlink $authfn or warn "Could not remove file '$authfn' Error: $!";
+		}
+		else
+		{
+			unlink $cmdfn or warn "Could not remove file '$cmdfn' Error: $!";
+			unlink $tfn or warn "Could not remove file '$tfn' Error: $!";
+			unlink $authfn or warn "Could not remove file '$authfn' Error: $!";
+			# worked? extract class, fieldnames
+			# produce hash for each class, array of subhashes for the rows
+			my ($classname, @fieldnames, %nicedata);
+			for my $line (@rawdata)
+			{
+				chomp $line;
+				# wmic may very well return utf-8 encoded data, e.g. for Caption in win32_operatingsystem on 6.0.6001
+				# so let's try to be nice and decode it into native unicode
+				my $validunicode = eval { decode('utf-8', $line, Encode::FB_CROAK); };
+				$line = $validunicode if (!$@);
+
+				# CLASS: Win32_PerfRawData_PerfOS_PagingFile
+				if ($line =~ /^class:\s+(\S+)\s*$/i)
 				{
-					@fieldnames = @columns;
+					$classname = $1;
+					undef @fieldnames;	# next line must be field names
 				}
 				else
 				{
-					return (error => "response contains data without classname!") if (!$classname);
-					return (error => "response data doesn't contain correct number of columns!")
-							if (@columns != @fieldnames);
+					# should be either list of names or list of values, with delim
+					my @columns = split(qr/$delim/, $line);
+					if (!@fieldnames)
+					{
+						@fieldnames = @columns;
+					}
+					else
+					{
+						return (error => "response contains data without classname!") if (!$classname);
+						return (error => "response data doesn't contain correct number of columns!")
+								if (@columns != @fieldnames);
 
-					# the only transformation we perform is replacing the common '(null)' value with undef.
-					my %thisrow = (map { $fieldnames[$_] => defined($columns[$_])
-																	 && $columns[$_] ne '(null)'? $columns[$_] : undef  }
-												 (0..$#fieldnames));
-					$nicedata{$classname} ||= [];
-					push @{$nicedata{$classname}}, \%thisrow;
+						# the only transformation we perform is replacing the common '(null)' value with undef.
+						my %thisrow = (map { $fieldnames[$_] => defined($columns[$_])
+																		&& $columns[$_] ne '(null)'? $columns[$_] : undef  }
+													(0..$#fieldnames));
+						$nicedata{$classname} ||= [];
+						push @{$nicedata{$classname}}, \%thisrow;
+					}
 				}
 			}
+			$result{ok} = 1;
+			$result{data} = \%nicedata;
 		}
-		$result{ok} = 1;
-		$result{data} = \%nicedata;
 	}
 
 	return %result;
