d0cde90f36aad856fedee5aee13a5da1a60d8f09
PallaviP <pallavip@firstwavecloud.com>
2023-12-05 13:18:59 +1000

    OMK-10543-Get Vue in dev for NMIS UX

---
+++ frontend_ts/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/src/main.ts',
+    },
+  },
+})
