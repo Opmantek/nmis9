ca063855e7ac66d0c31e91ef63fa936054a0b0a9
Arihant Bansal <arihant.bansal@firstwavecloud.com>
2024-05-10 14:33:00 +1000

    Force convert configuration props to strings

--- lib/NMISNG/Node.pm
+++ lib/NMISNG/Node.pm
@@ -581,14 +581,6 @@ sub configuration
 			}
 		}
 		
-		# PROP_STRINGS should always be a string
-		my @prop_strings = ('group','location');		
-		# convert given props to strings if they are integers.
-		foreach my $prop_name (@prop_strings){
-			if( defined($newvalue->{$prop_name}) && $newvalue->{$prop_name} =~ /^[0-9]+$/ ) { 
-				$newvalue->{$prop_name} = NMISNG::DB::make_string( $newvalue->{$prop_name} );
-			}		
-		}
 		$self->{_configuration} = $newvalue;
 		$self->_dirty( 1, 'configuration' );
 	}
@@ -1563,6 +1555,15 @@ sub save
 			);
 
 	map { $entry{$_} = $self->{_unknown}->{$_}; } (grep(!/^(_id|uuid|name|cluster_id|overrides|comments|configuration|activated|lastupdate|enterprise_service_tags)$/, keys %{$self->{_unknown}}));
+
+	# PROP_STRINGS should always be a string
+	my @prop_strings = ('group','location');		
+	# convert given props to strings if they are integers.
+	foreach my $prop_name (@prop_strings){
+		if( defined($entry{configuration}{$prop_name}) && $entry{configuration}{$prop_name} =~ /^[0-9]+$/ ) { 
+			$entry{configuration}{$prop_name} = NMISNG::DB::make_string( $entry{configuration}{$prop_name} );
+		}		
+	}
 
 	if ($self->is_new())
 	{
