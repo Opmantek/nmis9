c5540f36d4c8dfc2e84b23d5316dbe45fce62e3f
johns@opmantek.com <johns@opmantek.com>
2025-02-07 11:53:52 +1000

    fix spelling of MongoDB 6

--- installer_hooks/common_mongodb_6.sh
+++ installer_hooks/common_mongodb_6.sh
@@ -48,7 +48,7 @@ EOF
 				local RES;
 				# shellcheck disable=SC2039
 				local OUTPUT;				
-				echolog "Installing gnupg for MONGO6";
+				echolog "Installing gnupg for MongoDB 6";
 				execPrint "apt-get install -y gnupg 2>&1"||:;				
 				SOURCESFILE=/etc/apt/sources.list.d/mongodb-org-$DESIREDVER.list
 				[ ! -d /etc/apt/sources.list.d ] && mkdir -p /etc/apt/sources.list.d
