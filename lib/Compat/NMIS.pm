05d482a9a63565dd0a910916ef32262f10368a2a
John Sinclair <john.sinclair@firstwavecloud.com>
2025-07-16 15:36:55 +1000

    OMK-11928 MongoDB 7 build 9.6.2 release

--- lib/Compat/NMIS.pm
+++ lib/Compat/NMIS.pm
@@ -29,7 +29,7 @@
 package Compat::NMIS;
 use strict;
 
-our $VERSION = "9.6.2-MONGO7.5";
+our $VERSION = "9.6.2";
 
 use Time::ParseDate;
 use Time::Local;
--- lib/NMISNG.pm
+++ lib/NMISNG.pm
@@ -32,7 +32,7 @@
 # or directly via the object
 package NMISNG;
 
-our $VERSION = "9.6.2-MONGO7.5";
+our $VERSION = "9.6.2";
 
 use strict;
 use Data::Dumper;
