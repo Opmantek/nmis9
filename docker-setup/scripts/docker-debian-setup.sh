92447c84d57fa2aea9c0eb3791d1a68f8bb4097d
kishenk345 <kishen.kumar@firstwavecloud.com>
2025-10-23 11:13:35 +1100

    OMK-12085 Modify compose file, add .env and docker setup script (#126)
    * OMK-12085 Modify compose file, add .env and docker setup script
    
    * Fixes for env variable usage
    
    * Fix for consistent variable naming and volume naming
    
    * Change to just use internal docker network for communication between NMIS and Mongo

---
+++ docker-setup/scripts/docker-debian-setup.sh
@@ -0,0 +1,20 @@
+# This script performs general setup of Docker on Debian
+
+# Update repositories and install a few required packages
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the docker repository to apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update apt repositories to pick up new docker repository and install docker related packages
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
