8d13763a4665efc652464e90ed5a66e180aa5516
markdueck1 <38130900+markdueck1@users.noreply.github.com>
2026-04-01 13:24:53 +1100

    Layered configuration system for loading and saving  (#149)
    * Add config pruning
    
    * add exclusive keys which can only be in one config
    
    default config excluded, they are meant to be overridden from it
    
    * add cli actions for config
    
    writeConfData needs work
    
    * Update writeConfDefault so it doesn't try to update ENV or conf.d
    
    * ENV vars cant be changed by writeConfData either
    
    * if cluster_id is missing, write it to Config.nmis
    
    saving conf.d/* files for external systems to write to, they will
    expect them not to change
    
    * Update config.pl do display sources, getConfDeep instead of readConfData, invalidate cache option
    
    config.pl now understands where data comes from so it will allow updating site config
    
    readConfiData is now reconstructed using the loaded conf.
    
    we have to invlidated the cached config after saving a new one
    
    * add functions to determine if config has changed
    
    not used yet, worker/process that changes config will see the change
    and have it's cache invalidated, this will allow other processes to see
    that their config is also invalid.
    
    * Bug fixes
    
    - writeConfData now only writes data that is not default, config.pl doesn't have to figure this out anymore
    - ENV tracks which section values came from
    - make sure configs are not saved with macros expanded
    
    * show defaults in config GUI, simplify raw config
    
    change display name from site config to local config
    
    * refactor tests
    
    - remove need for sub processes
    - protect any running system from changes by using temp directories
    
    * Add config system docs, more system protection from tests
    
    * Address PR comments
    
    * change config file write to rename
    
    also refactored cluster_id write so it uses same code path
    
    * Updated docs
    
    * add config test to ci
    
    ---------
    
    Co-authored-by: Mark Dueck <mark.dueck@firstwave.com>

--- test/t_nmis_config.pl
+++ test/t_nmis_config.pl
@@ -4,7 +4,7 @@
 #
 #  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
 #
-#  This file is part of Network Management Information System (“NMIS”).
+#  This file is part of Network Management Information System ("NMIS").
 #
 #  NMIS is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -28,11 +28,11 @@
 #
 # *****************************************************************************
 
-# Test NMISNG fuctions
-# creates (and removes) a mongo database called t_nmisg-<timestamp>
-#  in whatever mongodb is configured in ../conf/
+# Test NMISNG config loading (layered config system)
+# Uses a temp directory for all config writes to protect system config.
+# The real conf-default/Config.nmis is symlinked as layer 1 defaults.
 use strict;
-our $VERSION = "1.1.0";
+our $VERSION = "4.0.0";
 
 use FindBin;
 use lib "$FindBin::Bin/../lib";
@@ -43,83 +43,339 @@ use NMISNG;
 use NMISNG::Log;
 use NMISNG::Util;
 use Compat::Timing;
-use IO::File;
+use File::Basename;
 use File::Path qw( make_path );
+use File::Copy;
+use File::Temp qw( tempdir );
 use Data::Dumper;
 
-my $t = Compat::Timing->new();
-my $time = $t->elapTime();
-my $C = NMISNG::Util::loadConfTable();
-$time = $t->elapTime() - $time;
-print $time. " time load config \n";
+# --- Setup temp directory ---
+# Structure: $tmpdir/conf/ (layer 2 + conf.d), $tmpdir/conf-default/ (symlink to real defaults)
+my $tmpdir = tempdir("nmis_test_XXXX", TMPDIR => 1, CLEANUP => 1);
+my $test_conf = "$tmpdir/conf";
+my $test_conf_default = "$tmpdir/conf-default";
+make_path($test_conf, "$test_conf/conf.d", $test_conf_default);
+symlink("$FindBin::Bin/../conf-default/Config.nmis", "$test_conf_default/Config.nmis")
+	or die "Cannot symlink defaults: $!";
 
-# log to stdout
-my $logger = NMISNG::Log->new( level => 'debug' );
-
-is($C->{'auth_expire'}, "+30min", "Config file loaded" );
-
-my $conf_d_dir = $C->{'<nmis_conf>'} . "/conf.d";
-if ( !-d $conf_d_dir ) {
-    make_path $conf_d_dir or die "Failed to create path: $conf_d_dir";
-}
-
-my $file = "$conf_d_dir/TEST.nmis";
-
-my $content = "%hash = (\'authentication\'=>{\'auth_expire\'=>\'+2min\',
-\'test\'=>2});";
-
-my $fn;
-open($fn, '>', $file) or die "Could not open file '$file' $!";
-
-print $fn $content;
-close $fn;
-
-sub cleanup_db
+# Redirect <nmis_var> to temp dir via conf.d override (persists across site config changes)
+my $test_var = "$tmpdir/var";
+make_path("$test_var/nmis_system");
 {
-    unlink $file;
-	# Remove conf file
+	my $dir_override_file = "$test_conf/conf.d/00_test_dirs.nmis";
+	open(my $fh, '>', $dir_override_file) or die "Cannot write dir overrides: $!";
+	print $fh Data::Dumper->Dump([{ 'directories' => { '<nmis_base>' => $tmpdir, '<nmis_var>' => $test_var } }], [qw(*hash)]);
+	close $fh;
 }
 
-$time = $t->elapTime();
-$C = NMISNG::Util::loadConfTable();
-$time = $t->elapTime() - $time;
-print $time. " time load new config \n";
+# Helper: force config reload from disk
+sub reload_config {
+	$NMISNG::Util::_config_cache_invalid = 1;
+	return NMISNG::Util::loadConfTable(dir => $test_conf);
+}
 
-is($C->{'auth_expire'}, "+2min", "External config file loaded" );
+# Helper: restore config from .bak file
+sub restore_config {
+	my $C = NMISNG::Util::loadConfTable(dir => $test_conf);
+	my $bak = $C->{configfile} . ".bak";
+	File::Copy::move($bak, $C->{configfile}) if -e $bak;
+	reload_config();
+}
 
-# Try to edit not editable configs
-$content = "%hash = (\'id\'=>{\'cluster_id\'=>\'FAIL\'});";
-#my $fn;
-open($fn, '>', $file) or die "Could not open file '$file' $!";
+my $test_file = "$test_conf/conf.d/TEST.nmis";
 
-print $fn $content;
-close $fn;
-$time = $t->elapTime();
-$C = NMISNG::Util::loadConfTable();
-$time = $t->elapTime() - $time;
-print $time. " time load new config \n";
+# --- Test 1: Basic config loading (defaults only, no site config) ---
+my $C = NMISNG::Util::loadConfTable(dir => $test_conf);
+is($C->{'auth_expire'}, "+30min", "Config file loaded from defaults");
+ok(defined $C->{'db_server'}, "db_server is present");
+ok(defined $C->{'<nmis_base>'}, "nmis_base directory macro is present");
 
-is($C->{'cluster_id'}, "a5159999-0d11-4bcb-a402-39a460012345", "Non modificable properties OK" );
+# --- Test 2: Caching - second call returns same ref ---
+my $C2 = NMISNG::Util::loadConfTable(dir => $test_conf);
+is($C, $C2, "Second loadConfTable call returns cached ref");
 
-# Test for replacing macros in the file
-open($fn, '>', $file) or die "Could not open file '$file' $!";
+# --- Test 3: Source tracking ---
+my $sources = NMISNG::Util::getConfigSources();
+ok(ref($sources) eq 'HASH', "getConfigSources returns hashref");
+ok(scalar keys %$sources > 0, "getConfigSources has entries");
 
-$content = "%hash = (\'authentication\'=>{\'auth_htpasswd_file\'=>\'<nmis_conf>/users.dat\'});";
-close $fn;
-$time = $t->elapTime();
-$C = NMISNG::Util::loadConfTable();
-$time = $t->elapTime() - $time;
-print $time. " time load new config \n";
-# Check values
-#print Dumper($C);
+my $db_source = NMISNG::Util::getConfigSources(key => 'db_server');
+ok(defined $db_source, "getConfigSources returns info for db_server");
+ok($db_source->{layer} >= 1 && $db_source->{layer} <= 4, "db_server has valid layer");
+ok(defined $db_source->{section}, "db_server has section info");
+is($db_source->{section}, "database", "db_server section is 'database'");
 
-# We need to add here the real values as we need to know if the values are correctly replaced
-is($C->{'auth_htpasswd_file'}, "/usr/local/nmis9_josunec/conf/users.dat", "Replacing Macros from external conf OK" );
-is($C->{'auth_htpasswd_file'}, $C->{'<nmis_conf>'}."/users.dat", "Replacing Macros from external conf OK" );
-is($C->{'syslog_log'}, "/usr/local/nmis9_josunec/logs/cisco.log", "Replacing Macros from master config OK" );
-is($C->{'syslog_log'}, $C->{'<nmis_logs>'}."/cisco.log", "Replacing Macros from master config OK" );
-# Read again to see if it is loading the cache
-#my $C = NMISNG::Util::loadConfTable();
+# --- Test 4: Hardcoded values tracked ---
+my $conf_source = NMISNG::Util::getConfigSources(key => 'conf');
+ok(defined $conf_source, "getConfigSources returns info for hardcoded 'conf'");
+is($conf_source->{source}, "hardcoded", "conf source is 'hardcoded'");
 
-cleanup_db();
+# --- Test 5: Macro replacement ---
+is($C->{'syslog_log'}, $C->{'<nmis_logs>'} . "/cisco.log", "Replacing macros from master config OK");
+
+# --- Test 6: cluster_id is generated when missing ---
+ok(defined $C->{cluster_id} && $C->{cluster_id} ne '', "cluster_id is present and non-empty");
+
+# --- Test 7: conf.d override of existing key ---
+{
+	open(my $fh, '>', $test_file) or die "Could not open $test_file: $!";
+	print $fh "%hash = ('authentication'=>{'auth_expire'=>'+2min'});\n";
+	close $fh;
+
+	$C = reload_config();
+	is($C->{auth_expire}, "+2min", "conf.d override of existing key works");
+}
+
+# --- Test 8: conf.d cannot add new keys (override-only) ---
+{
+	open(my $fh, '>', $test_file) or die "Could not open $test_file: $!";
+	print $fh "%hash = ('authentication'=>{'auth_expire'=>'+2min', 'brand_new_key'=>'should_not_appear'});\n";
+	close $fh;
+
+	$C = reload_config();
+	ok(!defined $C->{brand_new_key}, "conf.d cannot add new keys (override-only)");
+}
+
+# --- Test 9: conf.d override tracked as layer 3 ---
+{
+	my $s = NMISNG::Util::getConfigSources(key => "auth_expire");
+	is($s->{layer}, 3, "conf.d override tracked as layer 3");
+}
+
+# Clean up conf.d
+unlink $test_file;
+$C = reload_config();
+
+# --- Test 10: ENV override ---
+{
+	local $ENV{NMIS_DB_SERVER} = "testhost";
+	$C = reload_config();
+	is($C->{db_server}, "testhost", "ENV NMIS_DB_SERVER override works");
+}
+
+# --- Test 11: ENV override tracked as layer 4 ---
+{
+	local $ENV{NMIS_DB_SERVER} = "testhost";
+	$C = reload_config();
+	my $s = NMISNG::Util::getConfigSources(key => "db_server");
+	is($s->{layer}, 4, "ENV override tracked as layer 4");
+}
+$C = reload_config();
+
+# --- Test 12: ENV NMIS_URL_BASE maps to <url_base> ---
+{
+	local $ENV{NMIS_URL_BASE} = "/test-url";
+	$C = reload_config();
+	is($C->{'<url_base>'}, "/test-url", "ENV NMIS_URL_BASE maps to <url_base>");
+}
+$C = reload_config();
+
+# --- Test 13: stripDefaults returns valid structure ---
+{
+	my ($stripped, $removals) = NMISNG::Util::stripDefaults();
+	ok(ref($stripped) eq 'HASH', "stripDefaults returns hashref");
+	ok(ref($removals) eq 'ARRAY', "stripDefaults returns arrayref of removals");
+	my $has_sections = grep { ref($stripped->{$_}) eq 'HASH' } keys %$stripped;
+	ok($has_sections || !keys %$stripped, "stripDefaults result has section keys or is empty");
+	my $all_valid = 1;
+	for my $r (@$removals) {
+		$all_valid = 0 unless defined $r->{section} && defined $r->{key};
+	}
+	ok($all_valid, "stripDefaults removals all have section and key");
+}
+
+# --- Test 14: stripDefaults removals match defaults ---
+{
+	my $defaults = NMISNG::Util::getConfigDefaults();
+	my ($stripped, $removals) = NMISNG::Util::stripDefaults();
+	my $all_in_defaults = 1;
+	for my $r (@$removals) {
+		unless (ref($defaults->{$r->{section}}) eq 'HASH' && exists $defaults->{$r->{section}}{$r->{key}}) {
+			$all_in_defaults = 0;
+			last;
+		}
+	}
+	ok($all_in_defaults, "stripDefaults removals all exist in defaults");
+}
+
+# --- Test 15: stripDefaults with site config ---
+{
+	# Write a site config with a non-default value
+	my %site = ( "email" => { "mail_domain" => "custom.example.com" } );
+	NMISNG::Util::writeHashtoFile(file => "$test_conf/Config.nmis", data => \%site);
+	$C = reload_config();
+
+	my ($stripped, $removals) = NMISNG::Util::stripDefaults();
+	ok(ref($stripped->{email}) eq 'HASH' && $stripped->{email}{mail_domain} eq "custom.example.com",
+		"stripDefaults keeps non-default site key");
+
+	unlink "$test_conf/Config.nmis";
+	$C = reload_config();
+}
+
+# --- Test 16: stripDefaults: no empty sections ---
+{
+	my ($stripped, $removals) = NMISNG::Util::stripDefaults();
+	my $no_empty = 1;
+	for my $section (keys %$stripped) {
+		next unless ref($stripped->{$section}) eq 'HASH';
+		$no_empty = 0 unless keys %{$stripped->{$section}};
+	}
+	ok($no_empty, "stripDefaults: no empty sections in result");
+}
+
+# --- Test 17: Exclusive property in conf.d ignored when already in local config ---
+{
+	# Write site config with cluster_id
+	my %site = ( "id" => { "cluster_id" => "my-site-cluster-id" } );
+	NMISNG::Util::writeHashtoFile(file => "$test_conf/Config.nmis", data => \%site);
+
+	# Write conf.d with conflicting cluster_id
+	open(my $fh, '>', $test_file) or die "Could not open $test_file: $!";
+	print $fh "%hash = ('id'=>{'cluster_id'=>'FAIL_FROM_CONFD'});\n";
+	close $fh;
+
+	my @warnings;
+	local $SIG{__WARN__} = sub { push @warnings, $_[0] };
+	$C = reload_config();
+
+	is($C->{cluster_id}, "my-site-cluster-id", "Exclusive property cluster_id in conf.d ignored");
+	ok(grep(/Exclusive property/, @warnings), "Warning emitted for duplicate exclusive property");
+}
+
+# --- Test 18: ENV cannot override exclusive property already claimed ---
+{
+	local $ENV{NMIS_CLUSTER_ID} = "ENV_OVERRIDE";
+	my @warnings;
+	local $SIG{__WARN__} = sub { push @warnings, $_[0] };
+	$C = reload_config();
+
+	is($C->{cluster_id}, "my-site-cluster-id", "ENV exclusive property cluster_id ignored");
+	ok(grep(/Exclusive property/, @warnings), "Warning emitted for ENV exclusive property override");
+}
+
+# Clean up
+unlink $test_file;
+unlink "$test_conf/Config.nmis";
+$C = reload_config();
+
+# --- Test 19: ENV can add new keys not in config ---
+{
+	local $ENV{NMIS_BRAND_NEW_TEST_KEY} = "hello";
+	$C = reload_config();
+	is($C->{brand_new_test_key}, "hello", "ENV can add new keys not in config");
+}
+$C = reload_config();
+
+# --- Test 20: writeConfData returns error for modified ENV-sourced key ---
+{
+	local $ENV{NMIS_DB_SERVER} = "envhost";
+	$C = reload_config();
+	my ($rawdata, $fn) = NMISNG::Util::getConfDeep(only_local => 1);
+	$rawdata->{database}{db_server} = "changed_host";
+	my $error = NMISNG::Util::writeConfData(data => $rawdata);
+	like($error, qr/db_server/, "writeConfData returns error for modified ENV-sourced key");
+}
+$C = reload_config();
+
+# --- Test 21: writeConfData returns error for modified conf.d key ---
+{
+	open(my $fh, '>', $test_file) or die "Could not open $test_file: $!";
+	print $fh "%hash = ('authentication'=>{'auth_expire'=>'+5min'});\n";
+	close $fh;
+	$C = reload_config();
+
+	my ($rawdata, $fn) = NMISNG::Util::getConfDeep();
+	$rawdata->{authentication}{auth_expire} = "+99min";
+	my $error = NMISNG::Util::writeConfData(data => $rawdata);
+	like($error, qr/auth_expire/, "writeConfData returns error for modified conf.d key");
+}
+
+# --- Test 22: writeConfData allows writing normal keys ---
+{
+	my ($rawdata, $fn) = NMISNG::Util::getConfDeep();
+	$rawdata->{email}{mail_domain} = "test-changed.example.com";
+	my $error = NMISNG::Util::writeConfData(data => $rawdata);
+	ok(!$error, "writeConfData allows writing normal keys");
+
+	$C = reload_config();
+	is($C->{mail_domain}, "test-changed.example.com", "Written value is correct after reload");
+	restore_config();
+}
+
+# --- Test 23: writeConfData skips unchanged conf.d keys without error ---
+{
+	my ($rawdata, $fn) = NMISNG::Util::getConfDeep();
+	my $error = NMISNG::Util::writeConfData(data => $rawdata);
+	ok(!$error, "writeConfData skips unchanged conf.d keys without error");
+	restore_config();
+}
+
+# Clean up conf.d
+unlink $test_file;
+$C = reload_config();
+
+# --- Test 24: configChanged returns false when no change has occurred ---
+{
+	ok(!NMISNG::Util::configChanged(conf => $C), "configChanged returns false when no change has occurred");
+}
+
+# --- Test 25: configChanged returns true after marker is updated ---
+{
+	sleep(1);
+	my $marker = $C->{'<nmis_var>'} . "/nmis_system/config_changed";
+	make_path(File::Basename::dirname($marker));
+	open(my $fh, ">", $marker) or die "cannot write marker: $!";
+	print $fh time() . "\n";
+	close $fh;
+	ok(NMISNG::Util::configChanged(conf => $C), "configChanged returns true after marker is updated");
+}
+
+# --- Test 26: writeConfData preserves site keys and adds new overrides without defaults ---
+{
+	my $configfile = "$test_conf/Config.nmis";
+
+	# Write a known starting site config
+	my %initial = (
+		"email"  => { "mail_domain" => "mycompany.com", "mail_from" => 'test@mycompany.com' },
+		"system" => { "nmis_user" => "testuser" },
+	);
+	NMISNG::Util::writeHashtoFile(file => $configfile, data => \%initial);
+	$C = reload_config();
+
+	# Get full effective config and modify two default values
+	my ($full, undef) = NMISNG::Util::getConfDeep();
+	$full->{system}{ping_timeout} = "9999";
+	$full->{logging}{log_level} = "debug";
+
+	my $error = NMISNG::Util::writeConfData(data => $full);
+	ok(!$error, "writeConfData succeeds with mixed site + new overrides");
+
+	# Read back the file
+	my $written = NMISNG::Util::readFiletoHash(file => $configfile);
+
+	# Existing site keys preserved
+	is($written->{email}{mail_domain}, "mycompany.com", "Existing site key mail_domain preserved");
+	is($written->{email}{mail_from}, 'test@mycompany.com', "Existing site key mail_from preserved");
+	is($written->{system}{nmis_user}, "testuser", "Existing site key nmis_user preserved");
+
+	# New overrides added
+	is($written->{system}{ping_timeout}, "9999", "New override ping_timeout added");
+	is($written->{logging}{log_level}, "debug", "New override log_level added");
+
+	# Default values NOT in file
+	ok(!exists $written->{database}{db_port}, "Default db_port not in written file");
+	ok(!exists $written->{authentication}{auth_expire}, "Default auth_expire not in written file");
+
+	restore_config();
+}
+
+# --- Test 27: getConfigDefaults returns layer 1 data ---
+{
+	my $defaults = NMISNG::Util::getConfigDefaults();
+	ok(ref($defaults) eq 'HASH', "getConfigDefaults returns hashref");
+	ok(ref($defaults->{database}) eq 'HASH', "getConfigDefaults has database section");
+	ok(exists $defaults->{database}{db_server}, "getConfigDefaults has db_server in database");
+}
+
 done_testing();
